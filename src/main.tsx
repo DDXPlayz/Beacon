d03af77012d807993cdcbdb033afeda35ea93f6d
DDXPlayz <112699123+DDXPlayz@users.noreply.github.com>
2026-01-14 21:25:41 +0530

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import BeaconApp from "./BeaconApp.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(<BeaconApp />);

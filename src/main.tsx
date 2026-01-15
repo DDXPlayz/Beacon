bc7672b8be2ef16a834c731296d12d0bb9329c88
DDXPlayz <112699123+DDXPlayz@users.noreply.github.com>
2026-01-15 13:52:53 +0530

    Update src directory

--- src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import BeaconApp from "./BeaconApp.tsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<BeaconApp />);
